3d209cd160c673ec8f4a64411aa6cbe2017ba6b6
audio <audio@PST6917.sciences-po.fr>
2017-11-17 16:28:58 +0100

    MOdif premier graph

---
+++ élèves.js
@@ -0,0 +1,20 @@
+var eleve = [ 
+	{
+	nom : "Dumazet",
+	prénom : "Mathilde",
+	âge : 22,
+	pointure : 38,
+	cheveux : "bruns",
+	chauve: false,
+	myope: true,
+
+}, 
+{
+	nom : "Bekk-Day",
+	prénom : "Lawrence",
+	âge : 25, 
+	pointure: 45,
+	cheveux: "bruns", 
+	chauve: false, 
+	myope: true,
+}] 
